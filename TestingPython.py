9e6f84690ea82103307cbb034c296d56236b5cd3
Leslie <lesliew1993@gmail.com>
2020-04-10 14:46:57 -0400

    init

---
+++ TestingPython.py
@@ -0,0 +1,32 @@
+# print('Hello world!')
+
+# x = [1, 2, 3, 4, 5]
+
+# for num in x:
+#     print(num)
+
+# my_income = 100
+# tax_rate = 0.1
+# my_taxes = my_income * tax_rate
+# print(my_taxes)
+# print("testing these string's\n's\t's")
+# print("length", len(x))
+
+# string = "hello world"
+# print(string.split("e"))
+
+# print(set([1, 1, 2, 2]))
+# x = { 1, 2, 3 }
+# y = { 1: "one" }
+# print(type(x))
+# print(x.add(4))
+# print(x)
+# print(type(y))
+# print(set("Mississippi"))
+arr = [1, 2, 3]
+for num in arr:
+    print(num)
+
+print('looping is done!')
+
+
